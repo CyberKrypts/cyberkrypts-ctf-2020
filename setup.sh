fa4a4426fd26ca8c094f3fc4fafad385d5d9089d
CyberKrypts <67453333+CyberKrypts@users.noreply.github.com>
2020-07-02 16:45:53 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,7 @@
+apt-get update
+apt-get upgrade -y 
+apt-get install -y apache2 php  
 
+apt-get install -y php-pear php-dev tcl-expect-dev 
+pecl install channel://pecl.php.net/expect
+echo extension=expect.so >> /etc/php*/apache2/php.ini
